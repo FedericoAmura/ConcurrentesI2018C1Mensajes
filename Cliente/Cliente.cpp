b4834d5bebab86e50e1b5974e696e6d3f2be5443
Federico Amura <federico.amura@viapool.com>
2018-06-24 19:34:37 -0300

    Menu para el cliente agregado

--- Cliente/Cliente.cpp
+++ Cliente/Cliente.cpp
@@ -6,6 +6,40 @@ using namespace std;
 
 void Cliente::iniciar() {
     cout << "Iniciando en modo cliente" << endl;
+    char input = 'a';
+    while (input != '0') {
+        cout << endl;
+        cout << "------- MENU -------" << endl;
+        cout << "0: Salir" << endl;
+        cout << "1: Consultar personas" << endl;
+        cout << "2: Alta de persona" << endl;
+        cout << "Seleccione una opcion: ";
+        cin >> input;
+
+        switch (input) {
+            case '0':
+                cout << endl;
+                cout << "Se procedera a cerrar el cliente" << endl;
+                break;
+            case '1': {
+                cout << endl;
+                cout << "=============================================" << endl;
+                cout << "             Consulta de personas            " << endl;
+                cout << "=============================================" << endl;
+                break;
+            }
+            case '2': {
+                cout << endl;
+                cout << "=============================================" << endl;
+                cout << "               Alta de persona               " << endl;
+                cout << "=============================================" << endl;
+                break;
+            }
+            default:
+                cout << "Opcion no reconocida" << endl;
+                break;
+        }
+    }
 }
 
 Cliente::Cliente() {
